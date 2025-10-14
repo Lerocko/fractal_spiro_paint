cd0bac95bb6f80f0323ac4e83dafbfd5e8887fd3
Lerocko <L.mz@outlook.com>
2025-10-14 21:49:25 +1300

    Practicing with tkinter librery and learning

--- src/ui/paint_window.py
+++ src/ui/paint_window.py
@@ -2,6 +2,11 @@
 paint_window.py
 Module for managing the graphical user interface of the Fractal Spiro Paint app.
 """
+
+from tkinter import *
+from tkinter import ttk
+
+
 class PaintWindow:
     """
     Main application window for Fractal Spiro Paint.
