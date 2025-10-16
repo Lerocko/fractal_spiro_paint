3c4cba4408597522951341855537bce22605d8fc
Lerocko <L.mz@outlook.com>
2025-10-16 23:00:39 +1300

    It has adding some buttons on the frame_file and frame_tools

--- src/ui/paint_window.py
+++ src/ui/paint_window.py
@@ -34,6 +34,33 @@ class PaintWindow:
         canvas = Canvas(frame_canvas, bg='#f0f0f0', width=700, height=900, highlightthickness=0) # Canvas for drawing
         canvas.pack(pady=20, anchor="center") # Fill the frame for canvas completely
 
+        # Add buttons to the file frame with padding and styling
+        btn_file = Button(frame_file, text="File", bg= "#3c3c3c", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_file.pack(side=LEFT, padx=10, pady=5) # Pack the button to the left side of the frame
+        btn_edit = Button(frame_file, text="Edit", bg= "#3c3c3c", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_edit.pack(side=LEFT, padx=10, pady=5) # Pack the button to the left side of the frame
+        btn_view = Button(frame_file, text="View", bg= "#3c3c3c", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_view.pack(side=LEFT, padx=10, pady=5) # Pack the button to the left side of the frame
+        btn_save = Button(frame_file, text="Save", bg= "#3c3c3c", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_save.pack(side=LEFT, padx=10, pady=5) # Pack the button to the right side of the frame
+        btn_undo = Button(frame_file, text="Undo", bg= "#4b4b4b", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_undo.pack(side=LEFT, padx=10, pady=10) # Pack the button to the left side of the frame
+        btn_redo = Button(frame_file, text="Redo", bg= "#4b4b4b", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_redo.pack(side=LEFT, padx=10, pady=10) # Pack the button to the left side of the frame
+
+        # Add buttons to the tools frame with padding and styling
+        btn_fractal = Button(frame_tools, text="Fractal", bg= "#3c3c3c", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_fractal.pack(side=LEFT, padx=10, pady=10) # Pack the button to the left side of the frame
+        btn_spiro = Button(frame_tools, text="Spiro", bg= "#3c3c3c", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_spiro.pack(side=LEFT, padx=10, pady=10) # Pack the button to the left side of the frame
+        btn_color = Button(frame_tools, text="Color", bg= "#3c3c3c", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_color.pack(side=LEFT, padx=10, pady=10) # Pack the button to the left side of the frame
+        btn_thickness = Button(frame_tools, text="Thickness", bg= "#3c3c3c", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_thickness.pack(side=LEFT, padx=10, pady=10) # Pack the button to the left side of the frame
+        btn_line_style = Button(frame_tools, text="Line Style", bg= "#3c3c3c", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_line_style.pack(side=LEFT, padx=10, pady=10) # Pack the button to the left side of the frame
+        btn_clear = Button(frame_tools, text="Clear", bg= "#e81123", fg="#f1f1f1", relief=RAISED, bd=0, padx=10, pady=5)
+        btn_clear.pack(side=LEFT, padx=10, pady=10) # Pack the button to the left side of the frame
 
 
     def start(self):
