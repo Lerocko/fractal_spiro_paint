b1e85da00af976fb578a11cdb4ffbaffbd178aaa
Lerocko <L.mz@outlook.com>
2025-10-31 07:55:28 +1300

    Creation and structuring tools_manager.py module to start allowing drawing in canvas

---
+++ src/ui/tools_manager.py
@@ -0,0 +1,43 @@
+"""
+tools_manager.py
+Control and managing activeted tools and theyre staites
+"""
+import tkinter as tk
+from typing import Literal
+
+# =============================================================
+# Constants and global variables
+# =============================================================
+active_tool = None
+color = "white"
+width = 2
+draw_type = "line"
+eraser = False
+fill = False
+
+# =============================================================
+# Tools management functions
+# =============================================================
+def set_tools():
+    pass
+
+def get_tools():
+    pass
+
+def current_tool():
+    pass
+
+def current_color():
+    pass
+
+def current_width():
+    pass
+
+def current_type():
+    pass
+
+def current_eraser():
+    pass
+
+def current_fill():
+    pass
